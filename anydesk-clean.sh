7d12a8057a184530c81eb302efc63cb7927676a3
antun3s <antunes.dll@gmail.com>
2022-11-03 06:32:33 -0300

    action to remove unnamed contacts

--- anydesk-clean.sh
+++ anydesk-clean.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 anydeskuserconf=~/.anydesk/user.conf
 
+# make a backup before any modification
+cp $anydeskuserconf "$anydeskuserconf"-backup
+
 cleanline () {
     text=$1
     echo 'cleaning line: ' $text
     sed -i "s/$text.*/$text/g" $anydeskuserconf
 }
 
-cp $anydeskuserconf "$anydeskuserconf"-backup
+removeunnamedhosts () {
+    SAVEDHOSTS=`cat $anydeskuserconf | grep 'ad.roster.items=' | sed 's/ad.roster.items\=//g' | sed 's/\;/;\n/g' | grep -e '.*,.*,[a-zA-Z]' | sed ':a;N;$!ba;s/\n//g'`
+    sed -i '/ad.roster.items=;*/ad.roster.items=$SAVEDHOSTS' $anydeskuserconf 
+
+}
 
 cleanline "ad.session.audio.playback="
 cleanline "ad.session.follow_remote_cursor="
@@ -22,5 +29,7 @@ cleanline "ad.session.sync_clipbrd="
 cleanline "ad.session.touch_mode="
 cleanline "ad.session.viewmode="
 echo ""
+
+
 
 echo "your backup file is avaliable on $anydeskuserconf.backup"
